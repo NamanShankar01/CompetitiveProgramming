3c60508aa964e88728c703f8feb5e81cdc1c5204
NamanShankar01 <143345001+NamanShankar01@users.noreply.github.com>
2024-04-04 23:47:25 +0530

    Frog 1

---
+++ Educational DP Contest/A. Frog 1.cpp
@@ -0,0 +1,249 @@
+// Problem: A - Frog 1
+// Contest: AtCoder - Educational DP Contest
+// URL: https://atcoder.jp/contests/dp/tasks/dp_a
+// Memory Limit: 1024 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+int const mod = 1e9 + 7; // 998244353
+#define setbits(x) __builtin_popcountll(x)
+#define msb(x) (63 - __builtin_clzll(x))
+#define lsb(x) (__builtin_ffsll(x) - 1)
+#define f(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+const long double eps = 1e-9;
+long double pie = 3.14159265358979323846264338327950;
+#define ff first;
+#define ss second;
+#define fsort(vec) sort(vec.begin(), vec.end())
+#define rsort(vec) sort(vec.begin(), vec.end(), greater<int>())
+#define rev(vec) reverse(vec.begin(), vec.end())
+#define max_ele(vec) *max_element(vec.begin(), vec.end())
+#define min_ele(vec) *min_element(vec.begin(), vec.end())
+
+/*----------------------------------------------------------------------------------------------*/
+
+int hcf(int a, int b)
+{
+    if (a % b == 0)
+        return b;
+    return hcf(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / (hcf(a, b));
+}
+
+/*----------------------------------------------------------------------------------------------*/
+/*  DSU  */
+
+class DSU
+{
+    vector<int> par, sz;
+
+public:
+    DSU(int n)
+    {
+        par.resize(n + 1);
+        sz.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+            par[i] = i;
+    }
+
+    int findUpar(int node)
+    {
+        if (par[node] == node)
+            return node;
+        return par[node] = findUpar(par[node]);
+    }
+
+    void unionbysize(int u, int vec)
+    {
+        int upar_u = findUpar(u);
+        int upar_vec = findUpar(vec);
+        if (upar_u == upar_vec)
+            return;
+        if (sz[upar_u] >= sz[upar_vec])
+        {
+            par[upar_vec] = upar_u;
+            sz[upar_u] += sz[upar_vec];
+        }
+        else
+        {
+            par[upar_u] = upar_vec;
+            sz[upar_vec] += sz[upar_u];
+        }
+    }
+};
+
+/*----------------------------------------------------------------------------------------------*/
+
+int binexp(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    int res = binexp(a, b / 2);
+    if (b % 2 == 0)
+        return res * res;
+    else
+        return res * res * a;
+}
+
+int binexpm(int a, int b, int M)
+{
+    if (b == 0)
+        return 1;
+    int res = binexpm(a, b / 2, M);
+    if (b % 2 == 0)
+        return ((res % M) * (res % M)) % M;
+    else
+        return (((res % M) * (res % M) % M) * (a % M)) % M;
+}
+
+int moduloMultiplication(int a, int b, int mod) {
+    int res = 0;
+    a %= mod;
+    while (b) {
+        if (b & 1)
+            res = (res + a) % mod;
+        b >>= 1;
+        a = (a * 2) % mod;
+    }
+    return res;
+}
+ 
+int powermod(int x, int y, int p) {
+    int res = 1;
+    x = x % p;
+    if (x == 0) return 0;
+    while (y > 0) {
+        if (y & 1)
+            res = (res * x) % p;
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+ 
+string decToBinary(int n) {
+    string s = "";
+    while (n > 0) {
+        s = to_string(n % 2) + s;
+        n = n / 2;
+    }
+    return s;
+}
+ 
+int binaryToDecimal(string n) {
+    int dec_value = 0;
+    int base = 1;
+    int len = n.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (n[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+    return dec_value;
+}
+ 
+bool binarysearch(int arr[], int l, int h, int x) {
+    while (l <= h) {
+        int mid = l + (h - l) / 2;
+        if (arr[mid] == x)
+            return true;
+        else if (arr[mid] > x)
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return false;
+}
+ 
+
+/*----------------------------------------------------------------------------------------------*/
+// for ncr and factorial with modulo M
+// const int N1=3*1e5;
+// int fact[N1+1];
+// int inv[N1+1];
+// int ncr(int n, int r){
+//     if(r>n) return 0;
+//     int temp = fact[n];
+//     temp = (temp*inv[r]) % mod;
+//     temp = (temp*inv[n-r]) % mod;
+//     return temp;
+// }
+/*----------------------------------------------------------------------------------------------*/
+
+int32_t main()
+{
+
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // Sieve approach:
+
+    // int N=1e6;
+    // int spf[N+1];
+    // f(i,N+1) spf[i]=i;
+    // for(int i=2;i<N+1;i++){
+    //     if(spf[i]==i){
+    //         for(int j=i*i;j<N+1;j+=i){
+    //            if(spf[j] == j) spf[j]=i;
+    //         }
+    //     }
+    // }
+
+    // int N=1e6;
+    // bool isprime[N+1];
+    // isprime[0]=0;
+    // isprime[1]=0;
+    // for(int i=2; i*i<=N; i++)
+    // {
+    //   if(isprime[i]==1){
+    //      for(int j=i*i; j<=N; j+=i)
+    //        {
+    //           isprime[j]=0;
+    //        }
+    //    }
+    //}
+
+    // vector<int> primes;
+    // for(int i=2;i<N+1;i++) if(isprime[i]) primes.push_back(i);
+
+    // fact[0]=1;
+    // f1(i,N1) fact[i] = (fact[i-1]*i) % mod;
+    // f(i,N1+1) inv[i] = binexpm(fact[i], mod-2, mod);
+
+    /* Code Starts here */
+
+    /*
+            be carefull at the time of coding.......
+                first think what's your logic.......
+                    keep in mind about time complexity and keep patience.....
+                        patience is more important than your practice........
+    */
+    int t=1;
+    //cin >> t;
+    while (t--)
+    {
+       		int n;
+	    	cin>>n;
+	    	vector<int>vec(n+1);
+	    	for(int i=1;i<=n;i++)cin>>vec[i];
+	    	vector<int>dp(n+1,1e9);
+	    	dp[1]=0;
+	    	dp[2]=abs(vec[2]-vec[1]);
+	    	for(int i=3;i<=n;i++){
+	    		dp[i]=min(abs(vec[i]-vec[i-1])+dp[i-1],abs(vec[i]-vec[i-2])+dp[i-2]);
+	    	}
+	    	cout<<dp[n];
+    }
+    return 0;
+}
